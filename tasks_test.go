6a61482eaf8fbbc4eea9153cc2a03dac3f852339
MuradIsayev <muradisayevazer@gmail.com>
2024-02-28 10:10:46 +0400

    feat: add tasks test

---
+++ tasks_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestCreateTask(t *testing.T) {
+	mockStore := &MockStore{}
+	service := NewTasksService(mockStore)
+
+	t.Run("should return error if name is empty", func(t *testing.T) {
+
+		payload := &Task{
+			Name: "",
+		}
+
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/tasks", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/tasks", service.handleCreateTask)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusBadRequest {
+			t.Error("invalid status code, it should fail")
+		}
+	})
+
+	t.Run("should create a task", func(t *testing.T) {
+		payload := &Task{
+			Name:         "task 1",
+			ProjectID:    1,
+			AssignedToID: 12,
+		}
+
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/tasks", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/tasks", service.handleCreateTask)
+
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
+	})
+}
